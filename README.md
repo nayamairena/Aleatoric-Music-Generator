fbd408e9f7c41d659a323e1d52ca0e024cf1382a
Naya <44099724+nayamairena@users.noreply.github.com>
2022-05-24 19:01:38 -0700

    Messing around with adding hw1 code for sine.wav

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # HW4: Aleatoric
 
 ## Naya Mairena
+
+CS410P: Music, Sound, and Computers  
+May, 24th 2022
+
+Aleatoric randomized music generator. This program will continuously play random sequences of notes that are selected from a major scale to make it sound "less random".  
+The program can be ran with several commands:
+
+- `--root`
+- `--beats`
+- `--bpm`
+- `--ramp`
+- `--accent`
+- `--volume`
--- aleatoric.py
+++ aleatoric.py
@@ -2,8 +2,12 @@
 #import random
 #import sys
 #import time
+import pyaudio
+import wave
+import numpy as np
 import argparse
-
+"""
+#Parse through the command line arguments to allow the user to customize commands.
 parser = argparse.ArgumentParser(description='Play an aleatoric.')
 parser.add_argument('--root', dest='root', type = int, default=48,
                     help='root note of scale (default: 48)')
@@ -18,5 +22,47 @@ parser.add_argument('--accent', dest='accent', type = float, default=5.0,
 parser.add_argument('--volume', dest='volume', type = float, default=8.0,
                     help='Volume for the unaccented beats of each measure (default: 8.0)')
 args = parser.parse_args()
+"""
 
-print(args.root)
+#Create Sine wave
+#CONST variables to set for the audio files.
+SAMPLE_RATE = 48000
+CHANNELS = 1
+SAMPLE_SIZE = 2
+
+#Part 1: sine.wav
+#Use wave library to open and write to the WAV file.
+sine_wav = wave.open('sine.wav', 'wb')
+sine_wav.setnchannels(CHANNELS)
+sine_wav.setsampwidth(SAMPLE_SIZE)
+sine_wav.setframerate(SAMPLE_RATE)
+
+
+
+for i in range(10):
+    #Use numpy library linspace() to set samples and duration of 1 second.
+    samples = np.linspace(0, 1, SAMPLE_RATE)
+    #Set amplitude 
+    amp = 32767*0.25
+    #Get sine wave using numpy sin() function. 
+    sine_data = amp * np.sin(2 * np.pi * (i*50+1) * samples)
+    #Write the frames as 16-bit int to the WAV file.
+    sine_wav.writeframes(sine_data.astype(np.int16))
+sine_wav.close()
+
+
+wf = wave.open('sine.wav', 'rb')
+#Create PyAudio
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = wf.getframerate(),
+                output = True)
+data = wf.readframes(1024)
+while data != b'':
+    stream.write(data)
+    data = wf.readframes(1024)
+    
+
+stream.close()
+p.terminate()
